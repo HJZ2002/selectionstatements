3386e25214d5c463eacd288ae75e06789e695407
zaccarias <zaccariah23@gmail.com>
2022-12-07 19:04:56 +0800

    Initial commit

---
+++ selectionstatements.py
@@ -0,0 +1,9 @@
+firstside = int(input("Enter the first side"))
+secondside = int(input("Enter second side "))
+thirdside = int(input("Enter third side "))
+
+if firstside == thirdside and secondside == thirdside:
+    print("The triangle is eqauilateral")
+
+else:
+    print("The print is not equailateral")
